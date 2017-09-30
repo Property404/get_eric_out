a9237f1662f2610c021e8e8fa5f4379c1831fee5
Property404 <martinez.dagan@gmail.com>
2017-09-30 05:42:34 -0400

    Spaces to tabs

--- pi/cam.py
+++ pi/cam.py
@@ -4,6 +4,8 @@ import sys
 import logging as log
 import datetime as dt
 from time import sleep
+import serial
+ser = serial.Serial("/dev/cu.usbmodemFA141");
 
 # Basic setup
 cascPath = "haarcascade_frontalface_default.xml"
@@ -14,48 +16,50 @@ log.info("Starting video capture")
 video_capture = cv2.VideoCapture(0)
 anterior = 0
 while True:
-    if not video_capture.isOpened():
-        log.info('Unable to load camera.')
-        sleep(5)
-        pass
+	if not video_capture.isOpened():
+		log.info('Unable to load camera.')
+		sleep(5)
+		pass
 
     # Capture frame-by-frame
-    ret, frame = video_capture.read()
+	ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-    )
+	faces = faceCascade.detectMultiScale(
+		gray,
+		scaleFactor=1.1,
+		minNeighbors=5,
+		minSize=(30, 30)
+	)
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-	center_x = (2*x+w)/2
-	center_y = (2*y+h)/2
-	width = w
-	height = h
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-	cv2.circle(frame, (center_x, center_y), 10, (0, 0, 255), 2)
-	print([center_x, center_y])
-	break
+	# Draw a rectangle around the faces
+	for (x, y, w, h) in faces:
+		center_x = (2*x+w)/2
+		center_y = (2*y+h)/2
+		width = w
+		height = h
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+		cv2.circle(frame, (center_x, center_y), 10, (0, 0, 255), 2)
+		print([center_x, center_y])
+		if center_x > 350:
+			print("Please turn camera right");
+		elif center_x < 250:
+			print("Please turn camera left");
+		ser.write(b'yl\x00\x10')
 
-    if anterior != len(faces):
-        anterior = len(faces)
-        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+		if anterior != len(faces):
+			anterior = len(faces)
+
+	# Display the resulting frame
+	cv2.imshow('Video', frame)
 
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+	# Display the resulting frame
+	cv2.imshow('Video', frame)
 
 # When everything is done, release the capture
 video_capture.release()
